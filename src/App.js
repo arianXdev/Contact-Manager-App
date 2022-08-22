f4ba37558e6a9c3211b9a6a3b14159070aedde02
Arian <arianhosseini@outlook.com>
2022-08-23 01:53:44 +0430

    FontAwesome 6 added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { CssVarsProvider } from "@mui/joy/styles";
+import { Button } from "@mui/joy";
+
 import styles from "./App.module.css";
 
 const App = () => {
 	return (
 		<div className={styles.App}>
 			<h1>وب اپلیکیشن مدیریت مخاطبین</h1>
+			<CssVarsProvider>
+				<Button>Click</Button>
+			</CssVarsProvider>
 		</div>
 	);
 };
