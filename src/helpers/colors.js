62d1e13d86145b336873c67f4fb5977696301a90
Arian <arianhosseini@outlook.com>
2022-08-23 20:43:12 +0430

    Color Palette of Project created

---
+++ src/helpers/colors.js
@@ -0,0 +1,9 @@
+// Collor palette
+export const Background = "#282C34";
+export const Foreground = "#707175";
+export const Primary = "#0066FF";
+export const Green = "#00CEA3";
+export const Red = "#FF0D3E";
+export const Gray = "#A5A5A5";
+export const Cyan = "#DFF1FF";
+export const Yellow = "#f1fa8c";
