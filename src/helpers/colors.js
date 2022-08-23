df6746cbd1df07e2620016d0c43e5d2ab50e99ff
Arian <arianhosseini@outlook.com>
2022-08-23 21:33:08 +0430

    Some colors.js issues fixed

--- src/helpers/colors.js
+++ src/helpers/colors.js
@@ -1,9 +1,9 @@
 // Collor palette
-export const Background = "#282C34";
-export const Foreground = "#707175";
-export const Primary = "#0066FF";
-export const Green = "#00CEA3";
-export const Red = "#FF0D3E";
-export const Gray = "#A5A5A5";
-export const Cyan = "#DFF1FF";
-export const Yellow = "#f1fa8c";
+export const BACKGROUND = "#282C34";
+export const FOREGROUND = "#707175";
+export const PRIMARY = "#0066FF";
+export const GREEN = "#00CEA3";
+export const RED = "#FF0D3E";
+export const GRAY = "#A5A5A5";
+export const CYAN = "#DFF1FF";
+export const YELLOW = "#f1fa8c";
