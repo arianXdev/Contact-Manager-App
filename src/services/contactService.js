622b9451b3e0b6c3ab66379d7f9234e60862b0fb
Arian Hosseini <arianhosseini@outlook.com>
2022-09-17 01:00:05 +0430

    feat: added create/update/delete methods to work with API

--- src/services/contactService.js
+++ src/services/contactService.js
@@ -29,3 +29,24 @@ export const getGroup = (groupId) => {
 	const url = `${SERVER_URL}/groups/${groupId}`;
 	return axios.get(url);
 };
+
+// @desc Create a new contact
+// @route POST http://localhost:9000/contacts
+export const createContact = (contact) => {
+	const url = `${SERVER_URL}/contacts`;
+	return axios.post(url, contact);
+};
+
+// @desc Update a contact
+// @route PUT http://localhost:9000/contacts
+export const updateContact = (contact, contactId) => {
+	const url = `${SERVER_URL}/contacts/${contactId}`;
+	return axios.put(url, contact);
+};
+
+// @desc Delete a contact
+// @route DELETE http://localhost:9000/contacts/:contactId
+export const deleteContact = (contactId) => {
+	const url = `${SERVER_URL}/contacts/${contactId}`;
+	return axios.delete(url);
+};
