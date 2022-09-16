b524f419b71385315a0f73e015d833eddc5c02c0
Arian Hosseini <arianhosseini@outlook.com>
2022-09-17 00:39:37 +0430

    feat: added fetching data from local server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // eslint-disable-next-line
 import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
+import axios from "axios";
 
 // Import from components/index.jsx
 import { Contacts, AddContact, EditContact, ViewContact, Navbar } from "./components";
@@ -8,15 +9,35 @@ import { Contacts, AddContact, EditContact, ViewContact, Navbar } from "./compon
 import styles from "./App.module.css";
 
 const App = () => {
-	// eslint-disable-next-line
 	const [getContacts, setContacts] = useState([]);
-	// eslint-disable-next-line
 	const [loading, setLoading] = useState(false);
+	const [getGroups, setGroups] = useState([]);
+
+	useEffect(() => {
+		// Get data from server using Axios
+		const fetchData = async () => {
+			try {
+				setLoading(true); // showing the user loading data from server
+
+				const { data: contactsData } = await axios.get("http://localhost:9000/contacts");
+				const { data: groupsData } = await axios.get("http://localhost:9000/groups");
+
+				setContacts(contactsData);
+				setGroups(groupsData);
+
+				setLoading(false);
+			} catch (err) {
+				console.log(err.message);
+				setLoading(false);
+			}
+		};
+
+		fetchData();
+	}, []);
 
 	return (
 		<div className={styles.App}>
 			<Navbar />
-
 			<Routes>
 				<Route path="/" element={<Navigate to="/contacts" />} />
 				<Route path="/contacts" element={<Contacts contacts={getContacts} loading={loading} />} />
--- src/components/Contacts/Contact.jsx
+++ src/components/Contacts/Contact.jsx
@@ -4,27 +4,27 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import styles from "./Contact.module.css";
 
-const Contact = () => {
+const Contact = ({ contact: { fullName, email, photo, mobileNumber, job, group, id } }) => {
 	return (
-		<div className={styles.Contact}>
+		<div className={styles.Contact} id={id} group={group}>
 			<div className={styles.ContactRight}>
-				<img src="https://placehold.jp/178x178.png" alt="contact" className={styles.ContactImage} />
+				<img src={photo || "https://placehold.jp/178x178.png"} alt={fullName || "contact"} className={styles.ContactImage} />
 			</div>
 			<div className={styles.ContactLeft}>
 				<ul className={styles.ContactInfoList}>
 					<li className={styles.ContactItem}>
 						<span className={styles.ContactNameLabel}>نام:</span>
-						<h3 className={styles.ContactName}>آرین</h3>
+						<h3 className={styles.ContactName}>{fullName || "آرين"}</h3>
 					</li>
 
 					<li className={styles.ContactItem}>
 						<span className={styles.ContactMobileLabel}>موبایل:</span>
-						<h3 className={styles.ContactMobile}>09123456789</h3>
+						<h3 className={styles.ContactMobile}>{mobileNumber || "0912345679"}</h3>
 					</li>
 
 					<li className={styles.ContactItem}>
 						<span className={styles.ContactEmailLabel}>ایمیل:</span>
-						<h3 className={styles.ContactEmail}>example@example.com</h3>
+						<h3 className={styles.ContactEmail}>{email || "exmaple@exmaple.com"}</h3>
 					</li>
 				</ul>
 
