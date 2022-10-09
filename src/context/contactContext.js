3c64e5b2c3e3473feec7cb35a0b8fd9f9e550d7a
Arian Hosseini <arianhosseini@outlook.com>
2022-10-09 22:25:03 +0330

    refactor: removed useless default values and added setContacts function

--- src/context/contactContext.js
+++ src/context/contactContext.js
@@ -1,21 +1,18 @@
 import { createContext } from "react";
 
-
 export const ContactContext = createContext({
 	// Default values
 	loading: false,
 	setLoading: () => {},
 	contacts: [],
-	filteredContacts: [],
+	setContacts: () => {},
 	groups: [],
 	contact: {},
-	setContact: () => {},
 	contactQuery: {},
-	
+
 	// Functions and Event Handlers
 	onContactChange: () => {},
 	deleteContact: () => {},
-	updateContact: () => {},
 	createContact: () => {},
-	contactSearch: () => {}
+	contactSearch: () => {},
 });
