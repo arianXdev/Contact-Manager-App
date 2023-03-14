fbf5a137069236bcd3eb30e18c6fa9ee49e08674
Arian Hosseini <arianhosseini@outlook.com>
2023-03-14 22:39:52 +0330

    fix: fixed an issue

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 
 import { BrowserRouter } from "react-router-dom";
 
-import "./index.css";
 import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
